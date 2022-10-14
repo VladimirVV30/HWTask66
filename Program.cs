5aca59dd9631043f6dd994d4b865e81f16f1f05f
Vladimir <madfrog@e1.ru>
2022-10-14 23:34:50 +0300

    Created Task 66

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+// Выполнить с помощью рекурсии.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumRec(int M, int N)
+{
+    int sum = M;
+    if (M == N) return 0;
+    else
+    {
+        M++;
+        sum = M + SumRec(M, N);
+        return sum;
+    }
+}
+
+void SumNums(int M, int N)
+{
+    Console.Write(SumRec(M - 1, N));
+}
+
+SumNums(1, 15);
